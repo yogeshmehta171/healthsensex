9379ca48b338a558ef6d7738d0fca86a9ab6cba3
yogeshmehta171 <yogeshmehta0114@gmail.com>
2026-04-25 13:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 
 st.set_page_config(
     page_title="HealthSensex — National Health Portal",
-    page_icon="🏛️",
+    page_icon="🩺",
     layout="wide",
     initial_sidebar_state="expanded",
 )
